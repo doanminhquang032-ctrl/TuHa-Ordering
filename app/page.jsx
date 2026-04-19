accc586d13f2cc8e2d9b0bfc4564fde89bf60787
doanminhquang032-ctrl <doanminhquang032@gmail.com>
2026-04-19 23:46:32 +0700

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+'use client';
+import React from "react";
+
+export default function Page() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>🍜 Web Order chạy rồi!!!</h1>
+      <p>Nếu m thấy dòng này là thành công 😎</p>
+    </div>
+  );
+}
